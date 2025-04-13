359e557e436a54b4fb8fabd845a7770603ea5f20
Lina <linate.pa@gmail.com>
2025-04-13 14:10:15 +0300

    up

--- index.js
+++ index.js
@@ -579,4 +579,4 @@ function hero(bullets, dragons){
 
     findDifference = (a, b) => Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]);
 
-    console.log("-------Difference of Volumes of Cuboids----------");
+    console.log("-----------------");
